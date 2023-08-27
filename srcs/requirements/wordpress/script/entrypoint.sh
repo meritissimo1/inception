f53b8a1f3f623819e3be338350bc99e027ff5f67
meritissimo1 <marcelomoreira1905@gmail.com>
2023-08-27 01:08:50 -0300

    html to wordpress

--- srcs/requirements/wordpress/script/entrypoint.sh
+++ srcs/requirements/wordpress/script/entrypoint.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-if ! wp core is-installed --allow-root --path="/var/www/html"; then
+if ! wp core is-installed --allow-root --path="/var/www/wordpress"; then
     wp core install --url="marcrodr.42.fr" \
     --title="WP-CLI" \
     --admin_user=$MYSQL_USER \
     --admin_password=$MYSQL_PASSWD \
     --admin_email=marcrodr@42.student \
-    --path=/var/www/html --allow-root
+    --path=/var/www/wordpress --allow-root
 fi
 
 exec php-fpm7.4 --nodaemonize
